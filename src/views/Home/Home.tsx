ac4c419ce09e83a3209bee026cfd449469bcbe8d
Kaiz Hudda <kaiz.hudda@gmail.com>
2022-11-29 18:27:11 +1100

    simplify search logic

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, shallowEqual } from "react-redux";
 import { Customer } from "../../components/Customer/Customer";
 import {
@@ -20,23 +20,33 @@ const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const history = useHistory();
-  const customers: ICustomer[] = useSelector(
+  let customers: ICustomer[] = useSelector(
     (state: CustomerState) => state.customers,
     shallowEqual
   );
+  console.log("state", customers);
   const [filteredSearchResults, setFilteredSearchResults] =
     useState<ICustomer[]>(customers);
+
+  useEffect(() => {
+    setFilteredSearchResults(getFilteredCustomers(searchTerm));
+  }, [customers, searchTerm]);
+
+  const getFilteredCustomers = (searchTerm: string) => {
+    return customers.filter(
+      (customer) =>
+        customer.firstName.toLowerCase().includes(searchTerm) ||
+        customer.lastName.toLowerCase().includes(searchTerm)
+    );
+  };
 
   const onHandleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const value = e.target.value.toLowerCase().trim();
     setSearchTerm(value);
-    const result = customers.filter(
-      (customer) =>
-        customer.firstName.toLowerCase().includes(value) ||
-        customer.lastName.toLowerCase().includes(value)
-    );
-    setFilteredSearchResults(result);
+    customers = getFilteredCustomers(value);
+    console.log("filtered", customers);
+    setFilteredSearchResults(customers);
   };
 
   return (
@@ -62,14 +72,13 @@ const Home = () => {
           </TableRow>
         </thead>
         <tbody>
-          {filteredSearchResults &&
-            filteredSearchResults.map((customer: ICustomer) => (
-              <Customer
-                key={customer.id}
-                customer={customer}
-                removeCustomer={removeCustomer}
-              />
-            ))}
+          {filteredSearchResults.map((customer: ICustomer) => (
+            <Customer
+              key={customer.id}
+              customer={customer}
+              removeCustomer={removeCustomer}
+            />
+          ))}
         </tbody>
       </Table>
     </>
